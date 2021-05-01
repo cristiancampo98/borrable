ab7ca45f0a70431807e9bda1ff13eaf29636aec5
Diana Solano <dsolano@kubo.co>
2021-05-01 12:13:29 -0500

    [Add] Basic uses to views

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,29 @@
   <div class="container">
     <h1>Hola mundo</h1>
 
-    <Toast />
     <form @submit.prevent="greet">
       <InputText v-model="text" type="text" />
       <Button type="submit" label="Submit" />
     </form>
+
+    <div>
+      <span @click="$router.push(localePath({ path: 'auth-service' }))">{{
+        $t('index.goTo')
+      }}</span>
+      <br />
+      <nuxt-link :to="localePath('auth-service')">{{
+        $t('index.goTo')
+      }}</nuxt-link>
+    </div>
+
+    <div v-for="locale in availableLocales" :key="locale.code">
+      <img :src="locale.image" height="50" width="50" />
+      <nuxt-link :to="switchLocalePath(locale.code)"
+        >{{ locale.name }}<br
+      /></nuxt-link>
+    </div>
+
+    <Button type="submit" label="Mostrar alert" @click="showTestAlert" />
   </div>
 </template>
 
@@ -17,9 +35,20 @@ export default {
       text: null,
     }
   },
+  computed: {
+    availableLocales() {
+      return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale)
+    },
+  },
   methods: {
     greet() {
       this.$toast.add({ severity: 'info', summary: 'Hello ' + this.text })
+    },
+    showTestAlert() {
+      this.showAlert({
+        type: 'success',
+        message: 'Mensaje de prueba',
+      })
     },
   },
 }
