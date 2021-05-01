c786a7a2c4c2c9cfc892616604aceb7649efbba8
Diana Solano <dsolano@kubo.co>
2021-05-01 12:07:25 -0500

    [Add] Loading view

---
+++ components/general/Loading.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="loading" class="viewLoading">Cargando, por favor espere...</div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    loading: false,
+  }),
+  methods: {
+    start() {
+      this.loading = true
+    },
+    finish() {
+      this.loading = false
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.viewLoading {
+  position: fixed;
+  z-index: 1000;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(255, 255, 255, 0.596);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  img {
+    max-width: 700px;
+  }
+}
+</style>
